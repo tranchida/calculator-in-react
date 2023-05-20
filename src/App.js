9f4dacd6b07c82f8ad3a0288550a88e8c03fdf1a
Giampaolo Tranchida <giampaolo@tranchida.ch>
2023-05-20 19:33:13 +0200

    design

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Calculator from './calcultator';
 function App() {
   return (
     <div className="main">
+        <div className="box">
+          <center><h1 className="title">Points Calculator</h1></center>
+        </div>
       <Calculator/>
     </div>
   );
